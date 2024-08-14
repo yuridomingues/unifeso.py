1f5e08760cdf6e5bdc72942914be560b19ea50e5
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-08-14 20:55:46 -0300

    added exercise1 update

---
+++ aula_1/exercise_1.py
@@ -0,0 +1,14 @@
+# sum of 3 numbers
+
+def main() -> None:
+
+    num1: int = 30
+    num2: int = 20
+    num3: int = 50
+
+    result: int = num1 + num2 + num3
+
+    print(f"The sum of {num1}, {num2} and {num3} is = {result}")
+
+if __name__ == "__main__":
+    main()
