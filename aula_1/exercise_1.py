70b0ff6393e1161bd15f6e43b1659d993aed41cb
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-08-14 21:27:51 -0300

    new feature input

--- aula_1/exercise_1.py
+++ aula_1/exercise_1.py
@@ -1,12 +1,12 @@
-# sum of 3 numbers
+# 1- Write a program that reads a sequence of numbers and displays their sum.
 
 def main() -> None:
 
-    num1: int = 30
-    num2: int = 20
-    num3: int = 50
+    num1: int = int(input("Please, enter the first number: "))
+    num2: int = int(input("Please, enter the second number: "))
+    num3: int = int(input("Please, enter the third number: "))
 
-    result: int = num1 + num2 + num3
+    result: int = (num1 + num2 + num3)
 
     print(f"The sum of {num1}, {num2} and {num3} is = {result}")
 
