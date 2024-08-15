0cd6ce72b8898d3bff83cdd780909dae808a96f7
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-08-14 22:00:58 -0300

    new feature update

--- aula_1/exercise_1.py
+++ aula_1/exercise_1.py
@@ -1,14 +1,20 @@
 # 1- Write a program that reads a sequence of numbers and displays their sum.
 
 def main() -> None:
-
-    num1: int = int(input("Please, enter the first number: "))
-    num2: int = int(input("Please, enter the second number: "))
-    num3: int = int(input("Please, enter the third number: "))
-
-    result: int = (num1 + num2 + num3)
-
-    print(f"The sum of {num1}, {num2} and {num3} is = {result}")
-
+    quantity_sum: int = int(input("Please, enter the quantity of numbers you want to sum: "))
+    if quantity_sum < 0: 
+        print("Please, enter a valid number")
+    
+    else: 
+        result: int = 0 
+        i: int = 0
+        
+        while i < quantity_sum:
+            read_number: int = int(input(f"Please, enter number {i+1}: "))
+            result = result + read_number
+            i = i + 1
+        
+        print (f"The sum of the entered numbers is {result}")
+    
 if __name__ == "__main__":
     main()
