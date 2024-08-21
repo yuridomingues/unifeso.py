50e8e768383c14e13bb7bc5a76497dc4804e2f08
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-08-21 20:19:14 -0300

    added new exercise

---
+++ aula_2/sum_exemple.py
@@ -0,0 +1,9 @@
+def main() -> None:
+
+    number1: int = int(input("Enter the first number: "))
+    number2: int = int(input("Enter the second number: "))
+    sum: int = number1 + number2
+    print(f"The sum of {number1} and {number2} is {sum}")
+
+if __name__ == "__main__":
+    main()
