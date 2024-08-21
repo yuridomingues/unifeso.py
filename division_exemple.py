09a9ad12f4eceb2261a246bcf8035dc381d0597b
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-08-21 20:46:02 -0300

    added new exercise

---
+++ division_exemple.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    number1 = int(input("Enter the first number: "))
+    number2 = int(input("Enter the second number: "))
+
+    if number2 != 0:
+        division = number1 / number2
+        print(division)
+
+if __name__ == "__main__":
+    main()
