b6910535c2a16ff01de17eeaf4a47469fdc76536
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-08-28 21:32:50 -0300

    added new feature

--- aula_3/isEven.py
+++ aula_3/isEven.py
@@ -1,8 +1,8 @@
-list = list(range(-10, 51))
+# list = list(range(-10, 51))
 # Fastest method
 # print(list[::2])
 
-# Step by step method
+# Simple method
 #  for number in list:
 #      if number % 2 == 0:
 #          print(f"Number {number} is even.")
@@ -10,11 +10,32 @@ list = list(range(-10, 51))
 #         print(f"Number {number} is odd.")
 
 # Read negative method
+# for number in list:
+#     if number < 0:
+#         print(f"Number {number} is negative.")
+#     elif number % 2 == 0:
+#         print(f"Number {number} is even.")
+#     else:
+#         print(f"Number {number} is odd.")
+
+# Get the smallest and biggest even number in the sequence.
+list = list(range(-5, 6))
+smallest_even = max(list) + 1
+biggest_even = min(list) - 1
 
 for number in list:
     if number < 0:
         print(f"Number {number} is negative.")
     elif number % 2 == 0:
         print(f"Number {number} is even.")
+        # Get the smallest even number in the sequence.
+        if number < smallest_even:
+            smallest_even = number
+        # Get the largest even number in the sequence.
+        if number > biggest_even:
+            biggest_even = number
     else:
         print(f"Number {number} is odd.")
+        
+print(f"The smallest even number is {smallest_even}")
+print(f"The biggest even number is {biggest_even}")
