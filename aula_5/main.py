d5b2142cdcb605f0d7ada3a9e43ca8ca17db8b90
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-09-04 20:43:23 -0300

    added new code

--- aula_5/main.py
+++ aula_5/main.py
@@ -1,26 +1,58 @@
-LIMIT = 100 # Convention: Constants are written in uppercase
+MAX = 100
 
 def soma_for() -> int:
-    sum: int = 0
-
-    for number in range(0, LIMIT + 1):
-        sum = sum + number
-        print(number)
+    soma: int = 0
     
-    return sum
+    for numero in range(0, MAX + 1):
+        soma = soma + numero
+            
+    return soma
 
-def sum_while() -> int:
-    i: int = 0 # Counter variable
-    sum: int = 0
+def soma_for_break() -> int:
+    soma: int = 0
+    
+    for numero in range(0, MAX + 1):
+        if numero == 50:
+            break
+        
+        soma = soma + numero        
+            
+    return soma
 
-    while i <= LIMIT: 
-        sum = sum + i
-        i = i + 1
 
-def main():
-    # Can change the result to sum_for() to test the for loop
-    result = sum_while()
-    print(result)
+def soma_for_continue() -> int:
+    soma: int = 0
+    
+    for numero in range(0, MAX + 1):
+        if numero == 50:
+            continue
+        
+        soma = soma + numero        
+            
+    return soma
 
-if __name__ == "__main__":
+
+def soma_while() -> int:
+    i: int = 0 # variável contadora.
+    soma: int = 0
+    
+    while i <= MAX:
+        soma = soma + i
+        i = i + 1 # contando mais 1.
+    
+    return soma
+
+
+def leitura_teclado(letra: str) -> None:
+    while letra != 'e':
+        letra = input("Digite uma letra ('e' para sair): ")
+
+
+def main() -> None:
+    resultado = soma_for_continue()
+    print(f"Soma (continue): {resultado}.")
+    resultado = soma_for_break()
+    print(f"Soma (break): {resultado}.")
+    
+if _name_ == "_main_":
     main()
