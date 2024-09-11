17f83d6473def59f9a350e55d6d890ee0143fdea
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-09-11 19:51:20 -0300

    passou no quadro

---
+++ aula_6/template_programa.py
@@ -0,0 +1,23 @@
+def main() -> None:
+
+if__Name__= "__main__":
+    main()
+
+# From Random Import Random
+# Randint (1, 100)
+
+# 4 - Pelo menos:
+# 8 chars
+# 1 letra maiúscula
+# 1 numero
+# 1 char especial
+
+# 6- Criar um menu
+# 1 Tirar o saldo
+# 2 - Deposito
+# 3 - Saque
+# 4 - tirar extrato, usem o dicionario 
+# ex: extrato = Dict[str, List[float]]
+# extrato = ['D'] = []
+# extrato ['S'] = []
+# obs: saldo começa com 0 reais 
