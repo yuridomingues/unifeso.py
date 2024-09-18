9f72db1996ab6511193a0372df2cbf64d4db114c
Yuri Domingues <yuridomingues.contato@gmail.com>
2024-09-18 19:12:08 -0300

    updated feature

--- aula_3/string.py
+++ aula_3/string.py
@@ -1,12 +1,17 @@
 # Poor exemple code
 
-# lista = {1, 2, 3} * 5
-# for i, numero in enumerate (lista):
-#     if i < len (lista)-1:
-#     print (str(numero), end = ', ')
-# else:
-#     print (str(numero), end = '')
+ lista = {1, 2, 3} * 5
+ for i, numero in enumerate (lista):
+     if i < len (lista)-1:
+     print (str(numero), end = ', ')
+ else:
+     print (str(numero), end = '')
 
 # Good code
 
-print(','.join([str(i) for i in {1, 2, 3} * 5]))
+lista = {1, 2, 3} * 5
+lista_str = [str(i) for i in lista]
+
+resultado = ",".join(lista_str)
+
+
